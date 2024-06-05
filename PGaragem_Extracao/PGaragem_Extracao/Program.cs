243ff0da6ecfa201e32b17b73b42c5b82f03253b
Guilherme <g.satilio@gmail.com>
2024-06-05 08:52:52 -0300

    fix: ajuste na geração de arquivos XML, adicionado Root

--- PGaragem_Extracao/PGaragem_Extracao/Program.cs
+++ PGaragem_Extracao/PGaragem_Extracao/Program.cs
@@ -2,6 +2,7 @@
 using Models;
 using System.Configuration;
 using System.Reflection.PortableExecutable;
+using System.Xml.Linq;
 
 internal class Program
 {
@@ -18,28 +19,34 @@ internal class Program
         // Carros por Status
         using (var writer = new StreamWriter(ConfigurationManager.ConnectionStrings["XMLFileOutputStatus"].ConnectionString))
         {
+            var xml = new XElement("Root");
             foreach (var item in carFalse.CarServiceTable)
             {
-                writer.WriteLine(item.Car.GetXMLDocument());
+                xml.Add(item.Car.GetXMLDocument());
             }
+            writer.WriteLine(xml);
             writer.Close();
         }
         // Carros por Cor
         using (var writer = new StreamWriter(ConfigurationManager.ConnectionStrings["XMLFileOutputColor"].ConnectionString))
         {
+            var xml = new XElement("Root");
             foreach (var item in carColor.Car)
             {
-                writer.WriteLine(item.GetXMLDocument());
+                xml.Add(item.GetXMLDocument());
             }
+            writer.WriteLine(xml);
             writer.Close();
         }
         // Carros por Ano
         using (var writer = new StreamWriter(ConfigurationManager.ConnectionStrings["XMLFileOutputYear"].ConnectionString))
         {
+            var xml = new XElement("Root");
             foreach (var item in carYear.Car)
             {
-                writer.WriteLine(item.GetXMLDocument());
+                xml.Add(item.GetXMLDocument());
             }
+            writer.WriteLine(xml);
             writer.Close();
         }
     }
